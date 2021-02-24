8c4d2def3e50fa8b2a64e70c44ce652f23ce0dbe
MrDax0001 <maxfencing@web.de>
2021-02-24 21:51:41 +0100

    Implemented write to file method

--- json_safestate_generator.py
+++ json_safestate_generator.py
@@ -1,9 +1,11 @@
 """ Create JSON safelogs to keep track of paths not being backed up or cleaned
 
 Created on: 18.02.2021
-Last revision: 18.02.2021
+Last revision: 24.02.2021
 @author: Max Weise
 """
+import json
+
 from datetime import date
 from os import getcwd, mkdir, path
 
@@ -11,37 +13,65 @@ class JSON_File_Manager(object):
     """ Manage JSON safelogs to determine which paths had a aborted backup
         
         @params
+            current_working_path: str - Path wich had a aborted backup
         @author
             Max Weise
     """
-    """ Path to directory"""
-    def __init__(self):
-#        self.root = root # ? Whatfor
+
+    def __init__(self, current_working_path):
+        self.__safe_folder_name = 'json_safe_logs'
+        self.__current_working_path = current_working_path
         self.__date_of_creation = date.today()
-        self.__safe_path = path.join(getcwd(), 'json_safe_logs')  # Remember FileExistsError
+        self.__backup_location = getcwd() + self.__safe_folder_name
+        try:    # Create a directory to story json safe logs
+            mkdir(self.__safe_folder_name)
+        except FileExistsError:
+            print('Path allready exists, proceed as normal\n')
+        except Exception as e:
+            print(e)
 
     # Getter
     def get_date_of_creation(self):
         return self.__date_of_creation
 
-    def get_safe_path(self):
-        return self.__safe_path
+    def get_current_working_path(self):
+        return self.__current_working_path
 
-    
-    def safe_to_json(self):
-        dict = {
-            'date' : self.get_date_of_creation(),
-            'path' : self.get_safe_path(),
-            # Reason for json safe
+    def get_backup_location(self):
+        return self.__backup_location
+
+    def safe_to_json(self, reason: str) -> str:
+        safe_stats = {
+            'path' : self.get_current_working_path(),
+            'date' : str(self.get_date_of_creation()),
+            'reason for aboartion' : reason,
         }
-        # JSON dump
-        # Return json string, pass to filesafe method
 
-    def load_from_json(self):
+        return json.dumps(safe_stats)
+
+    def load_from_json(self, safed_json_sring: str) -> dict:
+        return json.loads(safed_json_sring)
+
+    def write_file(self, json_to_safe: str):
+        safe_location = self.get_backup_location()
+        name = str(self.get_date_of_creation()) + '_log.json'
+        try:
+            f = open(path.join(self.__safe_folder_name, name), "w")
+            f.write(json_to_safe)
+        except Exception as e:
+            print(e)
+        finally:
+            f.close()
+
+    def read_file(self):
         pass
-
+        
     def __str__(self):
-        pass
+        return f'cwd : {self.__current_working_path}'
+
+def test():
+    j = JSON_File_Manager(getcwd())
+    j.write_file(j.safe_to_json('test_reason'))
 
 if __name__ == '__main__':
-    pass
+    test()
