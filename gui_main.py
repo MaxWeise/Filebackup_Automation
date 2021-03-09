cbf497d2fad9b8870ab8c6acfc92b45ff01fc462
MrDax0001 <maxfencing@web.de>
2021-03-09 16:46:33 +0100

    reverted from json backup. The main programm still needs to be fleshed out

--- gui_main.py
+++ gui_main.py
@@ -10,24 +10,10 @@ from user_interface import GUI, Confirmation_Dialog
 from json_safestate_generator import JSON_File_Manager
 from file_backup_classes import File_Backup, Filetype_Backup 
 
+from os import path, listdir, remove
+
 def main():
     # TODO: Recover from json safe
-    j_manager = JSON_File_Manager()
-    list_of_safed_files = [f for f in listdir(j_manager.get_backup_location())]
-
-    if len(list_of_safed_files) != 0:
-        confirmation_window = Confirmation_Dialog()
-        if confirmation_window.get_confirmation_value():
-            loaded_safes = []
-            for this in list_of_safed_files:
-                safestate = j_manager.load_from_json(j_manager.read_file(this))     # get json string and parse it to a python dict
-                if safestate['backup procedure'] == 'file type backup':
-                    pass    # TODO: Implement file type backup
-                elif safestate['backup procedure'] is None:
-                    pass    # TODO: Implement file backup
-                remove(path.join(j_manager.get_backup_location(), this))            # Remove json file after it was dealt with
-    else:
-        print('No json files found\n')
 
     app = GUI()
     app.run()
