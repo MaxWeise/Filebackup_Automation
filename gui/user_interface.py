4cba9ec39f5118d6cbea2dad5c894e585473bb4f
MrDax0001 <maxfencing@web.de>
2021-03-25 22:34:30 +0100

    Some minor refactoring has been done. Goal is to minimize the number of methods in the code and remove methods which do the same but slightly different

--- gui/user_interface.py
+++ gui/user_interface.py
@@ -2,11 +2,11 @@
     this script is executed in the gui_main.py file
 
 Created on: 06.03.2021
-Last Revision: 21.03.2021
+Last Revision: 25.03.2021
 @author: Max Weise
 """
 
-# TODO: Refactor the code
+# TODO: Refactor the code => remove now obsolete methods
 
 from tkinter import *
 from tkinter import ttk
@@ -124,6 +124,18 @@ class GUI(__User_Gui):
     """
     def __init__(self):
         """ Initialize a GUI object""" 
+        # Strings in labels
+        self.display_source = StringVar()
+        self.display_source.set('Source :')
+        self.display_destin = StringVar() 
+        self.display_destin.set('Destination :')
+
+        # Global attributes
+        self.__left_margin = 20
+        self.__is_file_type_backup = False
+        self.__src_path = StringVar()
+        self.__dst_path = StringVar()
+
         # Create a non resizable window
         self.root = Tk()
         self.root.title('Backup Automation')
@@ -131,19 +143,6 @@ class GUI(__User_Gui):
         self.root.resizable(0, 0)
         self.back = Frame(master=self.root)
         self.back.pack(padx=5, pady=5)
-        
-        # Strings in labels
-        self.display_source = StringVar()
-        self.display_source.set('Source :')
-        self.display_destin = StringVar() 
-        self.display_destin.set('Destination :')
-
-        # Global attrebutes
-        self.__left_margin = 20
-        self.__procedure = StringVar()
-        self.__src_path = StringVar()
-        self.__dst_path = StringVar()
-        self.__program_parameters = None    #! Tuple of the form (source, dest, method of backup)
 
         # Labels
         self.header1 = Label(self.root, text='Please provide a source and destination folder')
@@ -187,19 +186,13 @@ class GUI(__User_Gui):
         """ Return the user given destination path."""
         return self.__dst_path.get()
 
-    def get_procedure(self) -> str:
-        """ Return the backup procedure chosen by the user."""
-        return self.__procedure.get()
+    def get__is_file_type_backup(self) -> bool:
+        """ Return if the backup procedure chosen by the user is file type backup."""
+        return self.__is_file_type_backup
 
     def get_program_params(self) -> tuple:
-        """ Return a tuple which contains all the data used by the programm
-
-            Tuple format: (source_path, destination_path, backup_procedure)"""
-        if self.__program_parameters is None:
-            print('Sorry, could not find this attrebute, maybe its value is None?')
-        else:
-            return self.__program_parameters
-
+        """ Return a tuple which contains all the data used by the programm."""
+        return (self.get_source_path(), self.get_destin_path(), get__is_file_type_backup())
 
     # Setter 
     def set_source_path(self, new_str: str):
@@ -210,37 +203,35 @@ class GUI(__User_Gui):
         """ Set the destination path."""
         self.__dst_path.set(new_str)
 
-    def set_procedure(self, new_str: str):
-        """ Set the backup procedure."""
-        self.__procedure.set(new_str)
+    def set__is_file_type_backup(self, new_val: bool):
+        """ Set whether or not the backup procedure is file type backup."""
+        self.__is_file_type_backup = new_val
 
-    def set_program_params(self, new_program_params: tuple):
-        """ Set the tuple wich yields all neccessary programm parameters."""
-        if type(new_program_params) is tuple:
-            self.__program_parameters = new_program_params
-        else:
-            print('Incorrect Type, the type must be tuple')
-
+    # Obsolet
     def set_display_source(self, new_str: str):
         """ Set the variable which displays the currently selected source path."""
         self.display_source.set(new_str)
         self.set_source_path(new_str)
 
+    # Obsolet
     def set_display_destination(self, new_str: str):
         """ Set the variable which displays the currently selected destination path."""
         self.display_destin.set(new_str)
         self.set_destin_path(new_str)
 
+    # Obsolet
     def __open_src(self):
         """ Open a filedialog to choose a source directory and set the attribute and labeltext accordingly."""
         self.set_source_path(askdirectory())
         self.display_source.set('Source : ' + self.get_source_path())
 
+    # Obsolet
     def __open_dst(self):
         """ Open a filedialog to choose a destination directory and set the attribute and labeltext accordingly."""
         self.set_destin_path(askdirectory())
         self.display_destin.set('Destination : ' + self.get_destin_path())
 
+    # Implement in parent
     def __stop_program(self):
         """ Close the GUI window."""
         self.root.destroy()
