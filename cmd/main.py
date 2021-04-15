90fa93c9e986d02e26af0386df9518a52ed16e89
MrDax0001 <maxfencing@web.de>
2021-04-15 22:21:01 +0200

    Implemented simple backup procedure. Removed unused functions

--- cmd/main.py
+++ cmd/main.py
@@ -1,48 +1,18 @@
 """ Automatically backup files
 
 Created on: 27.01.2021
-Last revision: 12.04.2021
+Last revision: 15.04.2021
 @author: Max Weise
 """
 
 from command_help import print_help_table
+import file_backup_classes as fb
 
 """ Note:
     - Inspect the first arg of command list and descide which method to call
     - if no known command -> trash rest of list
     - else -> following str will be flags for command execution
 """
-def f_file_backup(src=None):
-    # Copy full tree of src to dst | Backup full tree without requirements
-    if src is None:
-        src = input('Please provide a source path:\n>>> ')
-
-    dst = input('Please provide a destination path:\n>>> ')
-
-    File_Backup_Instance = File_Backup(src, dst)
-    print(File_Backup_Instance)
-    File_Backup_Instance.backup_tree()
-
-def f_ftype_backup(src=None):
-    # Copy specific files from scr to dst | Sort out unwanted files, delete them and copy the resulting tree
-    if src is None:
-        src = input('Please provide a source path:\n>>> ')
-
-    dst = input('Please provide a destination path:\n>>> ')
-    fltyp = input('Please provide a list of filetypes (only letters, no "."):\n>>> ')
-
-    ftype_backup = Filetype_Backup(src, dst, fltyp)
-    ftype_backup.dump_files()
-
-    if input(f'Confirm deleting  all unwanted files (Y/N):\n>>> ') == 'Y':
-        ftype_backup.garbage_collector.collect_garbage()
-        ftype_backup.backup_tree()
-    else:
-        print('Backup initialized. Saving state')
-        j_manager = JSON_File_Manager()
-        j_manager.set_current_working_path(ftype_backup.root)
-        reason = 'User interrupted backup'
-        j_manager.write_file(j_manager.safe_to_json(reason))
 
 def parse_commands(interface_arguments: str) -> list:
     """Take the input line of a user and separate all words into a list. """
@@ -51,25 +21,52 @@ def parse_commands(interface_arguments: str) -> list:
         argument_list.pop(len(argument_list)-1)
 
     return argument_list
+
+def ask_src_dst_paths() -> tuple:
+    """Return user-given source and destinaiton paths. """
+    user_scr = input('Please provide a source path\n>>> ')
+    user_dst = input('Please provide a destination path\n>>> ')
     
+    return user_scr, user_dst
+
+def ask_file_types() -> list:
+    """Return user-given list of filetypes. """
+    f_types = input('Please provide a list filetypes\n>>< ')
+
+    return f_types.split(' ')
+
 '''
     1. Input commands like Powershell or bash (split on space)
-    2. Logg activity using decorators
+    2. Log activity using decorators
+
+    TODO: Python 3.10 -> Switch case for better readability
 '''
 def main():
     while (user_in := input('>>> ')) != 'exit':
         command_list = parse_commands(user_in)
 
         if command_list[0] == 'backup':
-            # TODO: Implement backup
+            command_list.pop(command_list.index('backup'))
             if '-ft' in command_list:
                 command_list.pop(command_list.index('-ft'))
+                src_dst_tupel = ask_src_dst_paths()
+                backup = fb.Filetype_Backup(src_dst_tupel[0], src_dst_tupel[1], ask_file_types())
+               # TODO: Implement filetype backup procedure
+               #       Simplify FT_Backup class -> files get sorted out directly, delete will be specified by flag
+            elif '-f' in command_list:
+                command_list.pop(command_list.index('-f')) 
+                src_dst_tupel = ask_src_dst_paths()
+                backup = fb.File_Backup(src_dst_tupel[0], src_dst_tupel[1])
+                print(backup)
+                backup.backup_tree()
             else:
                 print('There has been a mistake, there was no option specified')
+
         elif command_list[0] == 'help':
             print_help_table()
         else: 
             print(f'Sorry, I dont know the command <{command_list[0]}>')
+            print('Please check the spelling and try again')
             print('Type <help> to get an overview of the commands')
 
 if __name__ == '__main__':
