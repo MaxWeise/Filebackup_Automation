ebd484587ae0a0f339a12719711060d5300d960c
MrDax0001 <maxfencing@web.de>
2021-05-28 19:25:47 +0200

    Decluttered the File_Backup Class

--- cmd/src/file_backup.py
+++ cmd/src/file_backup.py
@@ -2,14 +2,11 @@
     In both cases, the structure of the directory will be keept.
 
 Created: 15.02.2021
-Last revision: 10.05.2021
+Last revision: 28.05.2021
 @author: Max Weise
 """
-import os
-import shutil
 
 from distutils.dir_util import copy_tree
-from os import getcwd, mkdir, path, rename
 
 class File_Backup(object):
     """ Backup all files / directories in specified root dir to a specified destination
@@ -26,24 +23,21 @@ class File_Backup(object):
         self.root = root
         self.dest = destination
 
+
     def set_root(self, new_root:str) -> None:
+        """ Set the root attribute of an instance."""
         self.root = new_root
 
     def set_dest(self, new_dest:str) -> None:
+        """ Set the dest attribute of an instance."""
         self.dest = new_dest 
+
 
     def backup_tree(self) -> None:
         """Copy recursivly from self.root to self.dest. """
         copy_tree(self.root, self.dest)
 
+
     def __str__(self) -> str:
         """Print a humanly readable representation to the console. """
         return f'Backing up {self.root} into {self.dest}'
-
-def msg():
-    print('This module is not suposed to be run as a main module')
-    print('To debugg, please run the <test.py> module')
-
-
-if __name__ == '__main__':
-    msg()
