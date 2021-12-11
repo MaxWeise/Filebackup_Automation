b3567ced55d49006dd52047246696fb501e924dd
MaxWeise <max.weise@student.reutlingen-university.de>
2021-12-11 20:55:36 +0100

    Changed the constructor. Removed setters, as they are defined in the parentclass. Changed the __str__ method to be more informative.

--- src/backup_script/file_type_backup.py
+++ src/backup_script/file_type_backup.py
@@ -15,65 +15,65 @@ class File_Type_Backup(Backup):
         their types (file endings '.xxx').
 
         @params
-            root:         str  - root directory
-            destination:  str  - destination directory
-            __file_types: list - filetyes to backup
+            source:         str  - source directory
+            destination:    str  - destination directory
+            file_types:   list - filetyes to backup
     """
 
-    def __init__(self, source: str, destination: str, file_types: list):
-        """ Initialize object by giving it root, destination
+    source: str
+    destination: str
+    file_types: list[str]
+
+    # TODO: Refactor the constructor to an empty one
+    def __init__(self, source: str = None, destination: str = None, file_types: list = None):
+        """ Initialize object by giving it source, destination
             and file types to copy.
         """
-        self.__source = source
-        self.__destination = destination
-        self.__file_types = file_types
+        if source:
+            source = source
 
-    def set_root(self, new_source: str) -> None:
-        """ Set the root attribute to a new root."""
-        self.__source = new_source
+        if destination:
+            destination = destination
 
-    def get_root(self) -> str:
-        """ Return the root attribute."""
-        return self.__source
+        if file_types:
+            file_types = file_types
 
-    def set_dest(self, new_destination: str) -> None:
-        """ Set the dest attribute to a new dest."""
-        self.__destination = new_destination
+    def get_source(self) -> str:
+        """ Return the source attribute."""
+        return self.source
 
     def get_dest(self) -> str:
         """ Get the contents of the __file_types attribute."""
-        return self.__destination
+        return self.destination
 
-    def set__file_types(self, new_file_types: list) -> None:
-        """ Set the attribute __file_types to a specified list.
-            Mostly used for testing.
-        """
-        self.__file_types = new_file_types
+    def set_file_types(self, new_file_types: list) -> None:
+        """ Set the attribute file_types to a specified list. Mostly used for testing."""
+        self.file_types = new_file_types
 
-    def get__file_types(self) -> list:
-        """ Get the contents of the __file_types attribute."""
-        return self.__file_types
+    def get_file_types(self) -> list:
+        """ Get the contents of the file_types attribute."""
+        return self.file_types
 
     def __check_for_relevant_files(self, list_to_check: list) -> list:
-        """ Search a given list of files for files with relevant
-            filetypes.
-        """
-        return [f for f in list_to_check if f.split('.')[-1] in self.__file_types]
+        """ Search a given list of files for files with relevant filetypes."""
+        return [f for f in list_to_check if f.split('.')[-1] in self.file_types]
 
     def backup(self) -> None:
-        """ Copy specified files matching the sorting-criterium from root to destination."""
+        """ Copy specified files matching the sorting-criterium from source to destination."""
 
-        if not os.path.exists(self.get_root()):
-            raise ValueError('Root does not exist')
+        if not os.path.exists(self.get_source()):
+            raise ValueError('Source directory does not exist')
 
-        if os.path.isfile(self.get_root()):
-            raise ValueError('Root can not be a file')
+        if os.path.isfile(self.get_source()):
+            raise ValueError('Source directory can not be a file')
 
-        for _, _, files in os.walk(self.get_root()):
+        for _, _, files in os.walk(self.get_source()):
             if len(files) > 0:
                 for f in self.__check_for_relevant_files(files):
-                    copy(os.path.join(self.get_root(), f), self.get_dest())
+                    copy(os.path.join(self.get_source(), f), self.get_dest())
 
-    # def __str__(self) -> str:
-    #     """ Print a humanly readable representation to the console."""
-    #     return f'Backing up {self.root} into {self.dest} using {self.__file_types}'
+    def __str__(self) -> str:
+        """ Print a humanly readable representation to the console."""
+        return(
+            f'<{type(self)}> source: {self.source} | destination: {self.dest} | types: {self.file_types}'
+        )
