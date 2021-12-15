300f32970b589125097bec430cb3ce1f76435156
MaxWeise <max.weise@student.reutlingen-university.de>
2021-12-15 22:22:46 +0100

    Designed and implemented a text input box. Functionality of buttons still missing.

--- src/backup_script/tk_file_extention_dialog.py
+++ src/backup_script/tk_file_extention_dialog.py
@@ -12,12 +12,40 @@ class TextInputDialog:
     """ Display a GUI window to input file extentions."""
 
     # Attributes
-    title: str
+    exit_code: int
     file_extentions: list[str]
 
+    __EXIT_CODES: dict[str, int] = {
+        'NOTRUN' : -1,
+        'SUCCESS' : 0,
+        'ERROR' : 1,
+    }
+
     def __init__(self, title: str = 'Header') -> None:
-        self.title = title
+        self.root = tk.Tk()
+        self.root.title(title)
+        self._setup_gui(self.root)
+
         self.file_extentions = []
+        self.exit_code = self.__EXIT_CODES['NOTRUN']
+
+    def _setup_gui(self, tk_root) -> None:
+        """ Setup the tkinter GUI."""
+        tk_root.geometry('200x100')
+        tk_root.config(bg='skyblue')    # Color of root window
+
+        tk.Label(tk_root, text='Input Fily Extentions').pack(side=tk.TOP, padx=5, pady=5)
+
+        text_input_label = tk.Entry(tk_root)
+        text_input_label.pack(padx=5, pady=5)
+
+        # Submit the userinput
+        submit_button = tk.Button(tk_root, text='Submit')
+        submit_button.pack(side=tk.RIGHT, padx=20, pady=5)
+
+        # Cancle the userinput
+        cancle_button = tk.Button(tk_root, text='Cancle')
+        cancle_button.pack(side=tk.LEFT, padx=20, pady=5)
 
     def get_user_input(self) -> list[str]:
         """ Return the userinput as a list of strings."""
@@ -29,4 +57,17 @@ class TextInputDialog:
 
     def run(self) -> None:
         """ Run the mainloop to display the GUI window."""
+        # self.root.withdraw()
+        try:
+            self.root.mainloop()
+        except Exception as e:
+            self.root.quit()
+            print(e)
+
+    def exit_on_cancle(self) -> None:
+        """ Clear the userinput and set exitcode to 1 when user clicks on cacle."""
+        pass
+
+    def safe_user_input(self) -> None:
+        """ Safe the userinput and set exit code to 0."""
         pass
