f2edb5d65a90ead5f4e4f47e8981527963844740
MaxWeise <max.weise@student.reutlingen-university.de>
2021-12-16 21:44:22 +0100

    Implemented the functionality of the GUI. The string that gets inserted in the textfield will be split on spaces and can be returned by a getter method.

--- src/backup_script/tk_file_extention_dialog.py
+++ src/backup_script/tk_file_extention_dialog.py
@@ -6,6 +6,7 @@ created 15.12.2021
 """
 
 import tkinter as tk
+from tkinter.constants import END
 
 
 class TextInputDialog:
@@ -24,28 +25,44 @@ class TextInputDialog:
     def __init__(self, title: str = 'Header') -> None:
         self.root = tk.Tk()
         self.root.title(title)
-        self._setup_gui(self.root)
+        # self._setup_gui(self.root)
 
         self.file_extentions = []
         self.exit_code = self.__EXIT_CODES['NOTRUN']
 
-    def _setup_gui(self, tk_root) -> None:
-        """ Setup the tkinter GUI."""
-        tk_root.geometry('200x100')
-        tk_root.config(bg='skyblue')    # Color of root window
+    # def _setup_gui(self, tk_root) -> None:
+    #     """ Setup the tkinter GUI."""
+        self.root.geometry('200x100')
+        self.root.config(bg='skyblue')    # Color of root window
 
-        tk.Label(tk_root, text='Input Fily Extentions').pack(side=tk.TOP, padx=5, pady=5)
+        tk.Label(self.root, text='Input File Extentions').pack(side=tk.TOP, padx=5, pady=5)
 
-        text_input_label = tk.Entry(tk_root)
-        text_input_label.pack(padx=5, pady=5)
+        self.text_input_label = tk.Entry(self.root)
+        self.text_input_label.pack(padx=5, pady=5)
 
         # Submit the userinput
-        submit_button = tk.Button(tk_root, text='Submit')
+        submit_button = tk.Button(self.root, text='Submit', command=self._click_submit_buttton)
         submit_button.pack(side=tk.RIGHT, padx=20, pady=5)
 
         # Cancle the userinput
-        cancle_button = tk.Button(tk_root, text='Cancle')
+        cancle_button = tk.Button(self.root, text='Cancle', command=self._click_cancle_buttton)
         cancle_button.pack(side=tk.LEFT, padx=20, pady=5)
+
+    def _click_submit_buttton(self) -> None:
+        """ This method will execute when the user clicks the submit button on the GUI
+            It will safe the contents from the textfield as a list
+        """
+        self.file_extentions = self.text_input_label.get().split(' ')
+        self.exit_code = self.__EXIT_CODES['SUCCESS']
+        self.root.quit()
+
+    def _click_cancle_buttton(self) -> None:
+        """ This method will execute when the user clicks the cancle button on the GUI
+            It will clear the contents of the textfield.
+        """
+        self.file_extentions = []   # Explicitly setting the list to an empty one
+        self.exit_code = self.__EXIT_CODES['ERROR']
+        self.root.quit()
 
     def get_user_input(self) -> list[str]:
         """ Return the userinput as a list of strings."""
@@ -53,21 +70,8 @@ class TextInputDialog:
 
     def set_contents_input_dialog(self, input_text: str) -> None:
         """ Auxiliaray method to set the input of the dialog window."""
-        pass
+        self.text_input_label.insert(END, input_text)
 
     def run(self) -> None:
         """ Run the mainloop to display the GUI window."""
-        # self.root.withdraw()
-        try:
-            self.root.mainloop()
-        except Exception as e:
-            self.root.quit()
-            print(e)
-
-    def exit_on_cancle(self) -> None:
-        """ Clear the userinput and set exitcode to 1 when user clicks on cacle."""
-        pass
-
-    def safe_user_input(self) -> None:
-        """ Safe the userinput and set exit code to 0."""
-        pass
+        self.root.mainloop()
--- tests/test_tk_file_extention_dialog.py
+++ tests/test_tk_file_extention_dialog.py
@@ -21,11 +21,10 @@ class Test_TextInputDialog(TestCase):
         """ Setup an instance of the text input dialog."""
         self.__UNDER_TEST = TextInputDialog(title='Test Instance')
 
-    @unittest.skip('Implementation will follow!')
     def test_get_user_input(self) -> None:
         """ Test that the userinput is correct and gets returned as list of strings."""
-        self.__UNDER_TEST.set_contents_input_dialog('txt abc xx_xx')    # Mock the userinput
-        expected = ['txt', 'abc', 'xx_xx']
+        self.__UNDER_TEST.set_contents_input_dialog('Test Submit Button')    # Mock the userinput
+        expected = ['Test', 'Submit', 'Button']
 
         self.__UNDER_TEST.run()
         actual = self.__UNDER_TEST.get_user_input()
@@ -33,15 +32,23 @@ class Test_TextInputDialog(TestCase):
         self.assertTrue(len(actual) > 0)
         self.assertEqual(len(actual), 3)
         self.assertAlmostEqual(actual.sort(), expected.sort())
+        self.assertEqual(self.__UNDER_TEST.exit_code, 0)
 
-    @unittest.skip('Implementation will follow!')
     def test_cancle_button(self) -> None:
         """ Test the behaviour of the canlce button."""
-        pass
+        self.__UNDER_TEST.set_contents_input_dialog('Test Cancle Button')    # Mock the userinput
+        expected = []
 
-    def test_GUI_appearence(self) -> None:
-        """ Run this to see how the GUI looks."""
         self.__UNDER_TEST.run()
+        actual = self.__UNDER_TEST.get_user_input()
+
+        self.assertEqual(len(actual), 0)
+        self.assertEqual(actual, expected)
+        self.assertEqual(self.__UNDER_TEST.exit_code, 1)
+
+    # def test_GUI_appearence(self) -> None:
+    #     """ Run this to see how the GUI looks."""
+    #     self.__UNDER_TEST.run()
 
 
 if __name__ == '__main__':
